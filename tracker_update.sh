536410ea7c3e7c2a15e21694c14fd207583d457b
donfo22 <103877585+donfo22@users.noreply.github.com>
2022-06-23 23:34:32 +0800

    Update tracker_update.sh

--- tracker_update.sh
+++ tracker_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 killall aria2c
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
-if [ -z "`grep "bt-tracker" /root/.aria2/aria2.conf`" ]; then
+if [ -z "`grep "bt-tracker" /root/.aria2c/aria2.conf`" ]; then
     sed -i '$a bt-tracker='${list} /root/.aria2/aria2.conf
     echo add......
 else
